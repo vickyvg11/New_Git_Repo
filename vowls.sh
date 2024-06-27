61488f24359358599f7f4974a5cdbd1995a7eb36
Vikrant <vickyvg11@yahoo.com>
2024-06-27 18:35:43 +0530

    Edited the vowls.sh

--- vowls.sh
+++ vowls.sh
@@ -10,6 +10,6 @@ vowel_count=$(echo $word | tr -cd 'aeiou' | wc -c)
 
 var1=$(echo $word | tr -cd 'bcdfghjklmnpqrstvwxyz' | wc -c )
 
-echo "the vwoels counts:$vowel_count"
+echo "the owel count:$vowel_count"
 
 echo "the cons counts:$var1"
