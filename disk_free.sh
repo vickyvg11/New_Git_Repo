7366ccf58e2ee4f5e3fe506a923bc0e8f6eb6c31
Vikrant <vickyvg11@yahoo.com>
2024-06-27 18:09:17 +0530

    From Dev1

--- disk_free.sh
+++ disk_free.sh
@@ -5,7 +5,7 @@ disk_size=$(df -h | sed -n '2p' | awk '{print $5}' | sed 's/%//g')
 disk_name=$(df -h | sed -n '2p' | awk '{print $1}')
 
 if [[ $disk_size -gt 30 ]] ; then
-	echo "Sent Mail to register email ID"
+	echo "Sent  Mail to register email-ID"
 else
 	echo "Disk is not Full so no need to send mail"
 fi
